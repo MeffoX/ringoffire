8e34848ee4f0481f5e1256859befb7c4008bf1ab
Stefan <roth.neukirchen@googlemail.com>
2023-08-23 14:19:26 +0200

    added cards

---
+++ src/models/game.ts
@@ -0,0 +1,27 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0 ;
+
+    
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        this.shuffle(this.stack);
+    }
+
+
+    public shuffle(array: any[]): void {
+        let currentIndex = array.length, randomIndex;
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+    }
+}
